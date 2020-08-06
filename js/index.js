c7abc6a9181f8852f3e641de84ec2b408e08d614
Harrison Aguiar <harryaguiar@gmail.com>
2020-08-05 23:19:06 -0400

    added style all over

--- js/index.js
+++ js/index.js
@@ -42,10 +42,14 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 // Navigation Bar
+const headerTag = document.querySelector('header');
+headerTag.style.background = "grey";
+headerTag.style.padding = "10px";
+
 const aTag = document.querySelectorAll('a')
 let i = 0;
 aTag.forEach(a => {
-  aTag[i].style.color= 'red';
+  aTag[i].style.color= 'white';
   i++
   a.textContent = siteContent.nav[`nav-item-${i}`]
  
@@ -53,12 +57,19 @@ aTag.forEach(a => {
 console.log(aTag, 'navigation');
 
 // h1
+
 const h1Tag = document.querySelector('h1');
 h1Tag.textContent = siteContent.cta['h1'];
+h1Tag.style.background = "green";
+h1Tag.style.borderRadius = "10rem";
 
 // Button
 const buttonTag = document.querySelector('button');
 buttonTag.textContent = siteContent.cta['button'];
+buttonTag.style.color = "white";
+buttonTag.style.fontWeight = "bold";
+buttonTag.style.background = "red";
+buttonTag.style.borderRadius = "10rem";
 
 // cta image
 const image = document.getElementById('cta-img');
@@ -67,12 +78,17 @@ image.setAttribute('src', siteContent['cta']['img-src']);
 // h4s
 const h4s = document.querySelectorAll('h4');
 h4s[0].textContent = siteContent['main-content']['features-h4'];
+h4s[0].style.fontSize = "xx-large";
+h4s[0].style.color = "red";
 h4s[1].textContent = siteContent['main-content']['about-h4'];
+h4s[1].style.fontSize = "xx-large";
+h4s[1].style.color = "red";
 h4s[2].textContent = siteContent['main-content']['services-h4'];
 h4s[3].textContent = siteContent['main-content']['product-h4'];
 h4s[4].textContent = siteContent['main-content']['vision-h4'];
 h4s[5].textContent = siteContent.contact['contact-h4'];
-h4s[0].style.color = 'red';
+
+
 
 // main content p tags
 const ps = document.querySelectorAll('p');
@@ -88,6 +104,9 @@ ps[4].textContent = siteContent['main-content']['vision-content'];
 const mainImage = document.getElementById('middle-img');
 mainImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
 
+const mainContent = document.querySelector('.main-content');
+mainContent.style.background = "#FFF288";
+mainContent.style.paddingTop = "5px";
 
 // contact p
 ps[5].textContent = siteContent.contact['address'];
@@ -96,6 +115,12 @@ ps[7].textContent = siteContent.contact['email'];
 
 // footer p
 ps[8].textContent = siteContent.footer['copyright'];
+ps[8].style.color = "white";
+
+const footerTag = document.querySelector('footer');
+footerTag.style.background = "#909090";
+footerTag.style.padding = "10px"
+
 
 
 
